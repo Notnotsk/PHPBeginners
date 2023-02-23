beb3086eefbe5fa19f9ca8377d9596644f5715ac
Scott Knoll <scknoll88@gmail.com>
2023-02-22 18:23:45 -0600

    ep6

--- index.php
+++ index.php
@@ -1,31 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Document</title>
-    <style>
-      body {
-    display: grid;
-    place-items: center;
-    height: 100vh;
-    margin: 0;
-    font-family: sans-serif;
-      }
-    </style>
-  </head>
-  <body>
-    <?php
-      $name = "Dark Matter";
-      $read = false;
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Recommended Books</h1>]
 
-      if ($read) {
-          $message = "You have read $name";
-      } else {
-          $message = "You have NOT read $name";
-      }
-    ?>
-    <h1>
-      <?php echo $message; ?>
-    </h1>
-  </body>
+  <?php 
+    $books = [
+      "Do Androids Dream of Electric Sheep",
+      "The Langoliers",
+      "Hail Mary"
+      ]; 
+  ?>
+
+  <ul>
+    <?php foreach ($books as $book) : ?>
+      <li><?= $book ?></li>
+      <?php endforeach; ?> 
+  </ul>
+  
+</body>
 </html>
