ce33d069e7fe7adfddae000d58eafcfa1d67d3f7
Scott Knoll <scknoll88@gmail.com>
2023-02-23 13:02:20 -0600

    ep8

--- index.php
+++ index.php
@@ -10,23 +10,44 @@
       [
         'name' => 'Do Androids Dream of Electric Sheep?',
         'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
         'purchaseUrl' => 'http://example.com'
       ],
       [
         'name' => 'Project Hail Mary',
         'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+      ],
+      [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
         'purchaseUrl' => 'http://example.com'
       ],
     ];
+
+    function filterByAuthor($books, $author) {
+      $filteredBooks = [];
+
+      foreach ($books as $book) {
+        if ($book['author'] === $author) {
+          $filteredBooks[] = $book;
+        }
+      }
+      return $filteredBooks;
+    }
+
 ?>
 <ul>
-  <?php foreach ($books as $book) : ?>
+  <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
     <li>
       <a href="<?= $book['purchaseUrl'] ?>">
-      <?= $book['name']; ?>
+      <?= $book['name']; ?> (<?= $book['releaseYear'] ?>)
       </a>
     </li>
   <?php endforeach; ?>
 </ul>  
+
 </body>
 </html>
